a43d1c10772a829badaf029644740e20d263f192
ebccoats <coats.emma@gmail.com>
2026-09-08 17:26:59 -0400

    add 3 pt lighting, add puppet to scene

--- src/Engine.ts
+++ src/Engine.ts
@@ -1,10 +1,12 @@
 import * as THREE from 'three'
 import GUI from 'lil-gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { OrbitControls } from 'three/examples/jsm/Addons.js'
 
 
 const scene = new THREE.Scene()
 
-const fov = 35
+const fov = 25
 const aspect = window.innerWidth / window.innerHeight
 const near = 0.1
 const far = 100
@@ -13,66 +15,106 @@ const camera = new THREE.PerspectiveCamera( fov, aspect, near, far )
 camera.position.set(0, 0, 5)
 camera.lookAt(0, 0, 0)
 
+
 const hue = 0xFFFFFF
 const intensity = 3
 const light = new THREE.DirectionalLight(hue, intensity)
-light.position.set(0, 0, 3)
+light.position.set(1, 2, 3)
 scene.add(light)
 light.castShadow = true
+
+const light2 = new THREE.AmbientLight(hue, intensity / 4)
+scene.add(light2)
+
+const rimLight = new THREE.DirectionalLight(hue, intensity)
+rimLight.position.set(-1, -2, -5)
+scene.add(rimLight)
+rimLight.castShadow = true
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize( window.innerWidth, window.innerHeight )
 document.body.appendChild(renderer.domElement)
 
+// Make resizing work right
+let canvas = renderer.domElement
+canvas.style.width = "100%"
+canvas.style.height = "100%"
+canvas.style.display = "block"
+console.log(canvas.style)
+
+// orbit controls for click, drag to move camera
+const orbit = new OrbitControls(camera, renderer.domElement)
+orbit.target.set(0, 0, 0)
+orbit.update()
+
+
 let cube = new THREE.Mesh()
 const gui = new GUI()
 
-
-async function makeCube() {
-    const geometry = new THREE.BoxGeometry(1, 1, 1)
-    const material = new THREE.MeshStandardMaterial({ color: 0xffff00 })
-    cube = new THREE.Mesh( geometry, material )
-    cube.castShadow = true
-    await scene.add(cube)
-    console.log(cube)
-
-    makeGui()
-
-    function makeGui() {
-        // GUI setup (break into its own file)
-        gui.add( document, 'title' )
-
-        gui.add( cube.position, 'x')
-            .min(-2)
-            .max(2)
-            .step(0.01)
-            .onChange( value => {
-                cube.position.setX(value)
-            })
-        gui.add( cube.position, 'y')
-            .min(-2)
-            .max(2)
-            .step(0.01)
-            .onChange( value => {
-                cube.position.setY(value)
-            })
-        gui.add( cube.position, 'z')
-            .min(-2)
-            .max(2)
-            .step(0.01)
-            .onChange( value => {
-                cube.position.setZ(value)
-            })
+// function makeGui() {
+//     // GUI setup (break into its own file)
+//     gui.add( document, 'title' )
+//
+//     gui.add( puppet.position, 'x')
+//     .min(-2)
+//     .max(2)
+//     .step(0.01)
+//     .onChange( value => {
+//         puppet.position.setX(value)
+//     })
+//     gui.add( puppet.position, 'y')
+//     .min(-2)
+//     .max(2)
+//     .step(0.01)
+//     .onChange( value => {
+//         puppet.position.setY(value)
+//     })
+//     gui.add( cube.position, 'z')
+//     .min(-2)
+//     .max(2)
+//     .step(0.01)
+//     .onChange( value => {
+//         cube.position.setZ(value)
+//     })
+// }
 
 
-    }
+const loader = new GLTFLoader()
 
+function makePuppet() {
+    const puppetPath = new URL('./assets/puppet_re-rig.glb', import.meta.url).href
+    loader.load(puppetPath, function (gltf) {
+        gltf.scene.scale.setScalar(0.5)
+        gltf.scene.traverse(function (child) {
+            if (child.isSkinnedMesh) {
+                if (child.name === 'Mesh001') {
+                    let skinnedMesh = child
+                    skeleton = child.skeleton
+                    skeleton.bones.forEach(bone => {
+                        bone.name = bone.userData.name
+                    })
+
+                }
+            }
+
+
+        })
+        console.log(gltf.scene)
+        scene.add(gltf.scene)
+        gltf.scene.position.setY(-2.5)
+    })
 }
+
+let skeleton = new THREE.Skeleton()
+async function setup() {
+    makePuppet()
+
+    // makeGui()
+}
+
 function animate( time ) {
     renderer.render (scene, camera)
-    cube.rotation.x = time / 2000
-    cube.rotation.y = time / 1000
 }
 
-makeCube()
+setup()
 renderer.setAnimationLoop( animate )
